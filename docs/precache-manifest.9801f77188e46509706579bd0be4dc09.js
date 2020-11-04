52f6212ac44a30be9c70ff62c324145f30ad6d27
Alejandro Salguero <alejandrosalgueroq@hotmail.com>
2020-11-03 21:46:28 -0600

    docs added

---
+++ docs/precache-manifest.9801f77188e46509706579bd0be4dc09.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af33d9789e85af830612111b658f95cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3df919b1c65ce26a6a2c",
+    "url": "/static/css/main.3959e8a0.chunk.css"
+  },
+  {
+    "revision": "4b44d119c92c5fb0f102",
+    "url": "/static/js/2.9a812277.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9a812277.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3df919b1c65ce26a6a2c",
+    "url": "/static/js/main.e38ee68e.chunk.js"
+  },
+  {
+    "revision": "c8429c3d75b971d53245",
+    "url": "/static/js/runtime-main.7b6f9e01.js"
+  }
+]);
